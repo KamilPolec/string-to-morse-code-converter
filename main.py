e6e30e8f6cae90689b8939857f1e421f559c7690
Kamil Polec <kamilpolec14@gmail.com>
2024-02-23 16:33:56 +0000

    less messy method of creating a morse code dict

--- main.py
+++ main.py
@@ -1,54 +1,45 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# driver = webdriver.Chrome()
-# driver.get("https://www.sckans.edu/~sireland/radio/code.html")
-# morse_table = driver.find_element(By.CSS_SELECTOR, "tbody")
-# print(morse_table.text)
-# driver.quit()
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import json
 
-url = "https://www.sckans.edu/~sireland/radio/code.html"
 
-page = urlopen(url)
-# print(page)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+def get_morse_dic():
+    url = "https://www.sckans.edu/~sireland/radio/code.html"
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    soup = BeautifulSoup(html, 'html.parser')
+    morse_table = soup.find_all('td')
+    morse_list = [td.text.strip() for td in morse_table]
+    punctuation_list = [".", ",", ":", "?", "'", "-", "/", "(", '"']
+    punc_indx = 0
+    let_indx = 0
+    for index, val in enumerate(morse_list):
+        if index <= 207:
+            if len(val) == 1 or "*" in val or "-" in val and val != "X-ray":
+                morse_list[let_indx] = val
+                let_indx += 1
+        else:
+            if len(val) > 1 and "*" not in val and "-" not in val:
+                morse_list[index] = punctuation_list[punc_indx]
+                punc_indx += 1
+    del morse_list[let_indx:208]
+    morse_dic = dict(zip(morse_list[:-2:2], morse_list[1:-2:2]))
+    return morse_dic
 
-soup = BeautifulSoup(html, 'html.parser')
 
-# print(soup.prettify())
-morse_table = soup.find_all('td')
+morse_dic = get_morse_dic()
 
-morse_list = []
-for td in morse_table:
-    morse_list.append(td.text.strip())
+str_to_decode = input("Please type in the sentence you want decoded: ")
 
-letters = morse_list[:208:8]
-codes = morse_list[1:208:8]
-morse_dic = {letters[i]: codes[i] for i in range(len(letters))}
-numbers = morse_list[208:-2:4]
-numcodes = morse_list[209:-2:4]
-num_dic = {numbers[i]: numcodes[i] for i in range(len(numbers))}
-morse_dic.update(num_dic)
-punctuation_list = [".", ",", ":", "?", "'", "-", "/", "(", '"']
-puncodes = morse_list[211:-2:4]
-punctuation_dic = {punctuation_list[i]: puncodes[i] for i in range(len(punctuation_list))}
-morse_dic.update(punctuation_dic)
+chars_from_input = [char for char in str_to_decode.upper()]
 
-decode = input("Please type in the sentence you want decoded: ")
+word_in_morse = " ".join([morse_dic[char] for char in chars_from_input if char in morse_dic])
 
-letter_list = [letter for letter in decode.upper()]
+print(f"Your word/sentence in morse code is: {word_in_morse.replace('*', '.')}")
 
-coded_word = " ".join([morse_dic[letter] for letter in letter_list if letter in morse_dic])
+coded_char_list = word_in_morse.split(" ")
 
-print(f"Your word/sentence in morse code is: {coded_word.replace('*', '.')}")
-
-coded_letters = coded_word.split(" ")
-# print(coded_letters)
-
-# print(list(morse_dic.keys())[list(morse_dic.values()).index(code)])
-
-uncoded_list = [list(morse_dic.keys())[list(morse_dic.values()).index(code)] for code in coded_letters if code in morse_dic.values()]
-
-print(" ".join(uncoded_list))
+# Decodes the code by selecting the uncoded characters in morse_dic by the index of the corresponding morse value
+# uncoded_list = [list(morse_dic.keys())[list(morse_dic.values()).index(morse_char)] for morse_char in coded_char_list if morse_char in morse_dic.values()]
+# print(" ".join(uncoded_list))
